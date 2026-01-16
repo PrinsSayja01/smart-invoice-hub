ff25e2e625eae27ded51b75ec1f75c3e2e08c947
PrinsSayja01 <162360493+PrinsSayja01@users.noreply.github.com>
2026-01-16 17:12:55 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDFTInK3g1RHfRNgbbHCT93OGOd_piSCkY",
+  authDomain: "invoiceai-f96e5.firebaseapp.com",
+  projectId: "invoiceai-f96e5",
+  storageBucket: "invoiceai-f96e5.firebasestorage.app",
+  messagingSenderId: "9494668535",
+  appId: "1:9494668535:web:ab553dc701dc377d075ff2",
+  measurementId: "G-9LJD26QE8G"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const analytics = getAnalytics(app);
