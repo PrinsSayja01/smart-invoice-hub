13ad965acafeecdb3c603dcbc964ebb2efe25db9
PrinsSayja01 <162360493+PrinsSayja01@users.noreply.github.com>
2026-01-31 15:48:13 +0100

    Update cors.ts

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,4 +1,4 @@
-export const corsHeaders = {
+export const corsHeaders: Record<string, string> = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
   "Access-Control-Allow-Methods": "POST, OPTIONS",
